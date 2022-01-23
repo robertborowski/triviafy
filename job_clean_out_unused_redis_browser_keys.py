0917cced1040f7b8166a16b666e7f8bd71b7be5b
robert borowski <rborowski141@gmail.com>
2022-01-23 10:07:26 -0500

    automate job to clean out redis cookie keys

---
+++ job_clean_out_unused_redis_browser_keys.py
@@ -0,0 +1,70 @@
+# -------------------------------------------------------------- Imports
+from backend.db.connection.postgres_connect_to_database import postgres_connect_to_database_function
+from backend.db.connection.postgres_close_connection_to_database import postgres_close_connection_to_database_function
+from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
+from backend.db.connection.redis_connect_to_database import redis_connect_to_database_function
+import json
+from backend.db.queries.select_queries.select_queries_triviafy_user_login_information_table_slack.select_triviafy_user_login_information_table_slack_all_team_channel_combos import select_triviafy_user_login_information_table_slack_all_team_channel_combos_function
+
+# -------------------------------------------------------------- Main Function
+def job_clean_out_unused_redis_browser_keys_function():
+  localhost_print_function('=========================================== job_clean_out_unused_redis_browser_keys_function START ===========================================')
+
+  # ------------------------ DB Conection START ------------------------
+  # Connect to Postgres database
+  postgres_connection, postgres_cursor = postgres_connect_to_database_function()
+  # ------------------------ DB Conection End ------------------------
+
+
+  # ------------------------ Get Distinct Team Channel Combo START ------------------------
+  team_channel_arr = select_triviafy_user_login_information_table_slack_all_team_channel_combos_function(postgres_connection, postgres_cursor)
+  
+  team_id_set = {''}
+  channel_id_set = {''}
+
+  for i in team_channel_arr:
+    team_id = i[0]
+    channel_id = i[1]
+    if team_id not in team_id_set:
+      team_id_set.add(team_id)
+    if channel_id not in channel_id_set:
+      channel_id_set.add(channel_id)
+  
+  team_id_set.remove('')
+  channel_id_set.remove('')
+  # ------------------------ Get Distinct Team Channel Combo END ------------------------  
+
+
+  # ------------------------ DB Close Conection START ------------------------
+  # Close postgres db connection
+  postgres_close_connection_to_database_function(postgres_connection, postgres_cursor)
+  # ------------------------ DB Close Conection END ------------------------
+
+
+  # ------------------------ Redis Delete START ------------------------
+  # Connect to redis database pool (no need to close)
+  redis_connection = redis_connect_to_database_function()
+  redis_keys = redis_connection.keys()
+
+  for key in redis_keys:
+    if 'user-slack_' in str(key) or 'aa_foo' in str(key) or 'localhost_' in str(key):
+      localhost_print_function('skipping key: {}'.format(key))
+      continue
+    else:
+      value = redis_connection.get(key).decode('utf-8')
+      user_nested_dict = json.loads(value)
+      redis_slack_team_id = user_nested_dict['slack_team_id']
+      redis_slack_channel_id = user_nested_dict['slack_channel_id']
+      redis_user_email = user_nested_dict['user_email']
+      if redis_slack_team_id not in team_id_set and redis_slack_channel_id not in channel_id_set:
+        redis_connection.delete(key)
+        localhost_print_function('deleted logged in user from Redis. Email: {}'.format(redis_user_email))
+  # ------------------------ Redis Delete END ------------------------
+
+  localhost_print_function('=========================================== job_clean_out_unused_redis_browser_keys_function END ===========================================')
+  return True
+
+
+# ---------------------------------------------------------------------------------------------------------------------------- Job to Run The Main Function
+if __name__ == "__main__":
+  job_clean_out_unused_redis_browser_keys_function()
