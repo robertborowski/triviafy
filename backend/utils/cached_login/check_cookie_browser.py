deffc53787019c04e63408d847094329abd6da67
robert borowski <rborowski141@gmail.com>
2022-06-15 07:45:59 -0400

    comments to separate cookie parts:

--- backend/utils/cached_login/check_cookie_browser.py
+++ backend/utils/cached_login/check_cookie_browser.py
@@ -8,10 +8,12 @@ from backend.utils.localhost_print_utils.localhost_print import localhost_print_
 def check_cookie_browser_function():
   localhost_print_function('=========================================== check_cookie_browser_function START ===========================================')
   
-  # Connect to redis database pool (no need to close)
+  # ------------------------ Connect to redis START ------------------------
   redis_connection = redis_connect_to_database_function()
+  # ------------------------ Connect to redis END ------------------------
   
-  # -------------------------------------------------------------- Running on localhost
+  
+  # ------------------------ Get cookie from localhost START ------------------------
   server_env = os.environ.get('TESTING', 'false')
   # If running on localhost
   if server_env == 'true':
@@ -23,8 +25,10 @@ def check_cookie_browser_function():
     except:
       localhost_print_function('=========================================== check_cookie_browser_function END ===========================================')
       return redirect('/', code=302)
+  # ------------------------ Get cookie from localhost END ------------------------
 
-  # -------------------------------------------------------------- NOT running on localhost
+  
+  # ------------------------ Get cookie from production START ------------------------
   else:
     try:
       get_cookie_value_from_browser = request.cookies.get('triviafy_browser_cookie')
@@ -32,6 +36,7 @@ def check_cookie_browser_function():
     except:
       localhost_print_function('=========================================== check_cookie_browser_function END ===========================================')
       return redirect('/', code=302)
+  # ------------------------ Get cookie from production END ------------------------
 
   
   localhost_print_function('=========================================== check_cookie_browser_function END ===========================================')
