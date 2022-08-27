d2230273f9f9eb707eee1117943726579882faa7
robert borowski <rborowski141@gmail.com>
2022-08-27 14:27:14 -0400

    footer includes test libraru

--- static/javascript/components/candidates/footer/not_signed_in/footer_candidates_not_signed_in.js
+++ static/javascript/components/candidates/footer/not_signed_in/footer_candidates_not_signed_in.js
@@ -27,6 +27,7 @@ class FooterNotSignedInClassCandidates extends HTMLElement {
         <li class="footer-sub-title-item-3 footer-sub-title-item-position-3"><a href="${this.getAttribute("link_candidates_about_js")}" class="footer-a-3">About</a></li>
         <li class="footer-sub-title-item-3 footer-sub-title-item-position-3"><a href="${this.getAttribute("link_candidates_faq_js")}" class="footer-a-3">FAQ</a></li>
         <li class="footer-sub-title-item-3 footer-sub-title-item-position-3"><a href="${this.getAttribute("link_candidates_pricing_js")}" class="footer-a-3">Pricing</a></li>
+        <li class="footer-sub-title-item-3 footer-sub-title-item-position-3"><a href="${this.getAttribute("link_candidates_library_js")}" class="footer-a-3">Test library</a></li>
         <li class="footer-sub-title-item-3 footer-sub-title-item-position-3"><a href="#" class="footer-a-3">Login</a></li>
         <li class="footer-sub-title-item-3 footer-sub-title-item-position-3"><a href="#" class="footer-a-3">Create Account</a></li>
       </ul>
