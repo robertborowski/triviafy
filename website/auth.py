676c2f9bfeb855b85063ca181686e459b15a7f10
robert borowski <rborowski141@gmail.com>
2022-08-30 20:34:37 -0400

    discussed flashing before talking about sqlalchemy

--- website/auth.py
+++ website/auth.py
@@ -6,7 +6,7 @@
 
 
 # ------------------------ imports start ------------------------
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 # ------------------------ imports end ------------------------
 
 
@@ -16,7 +16,7 @@ auth = Blueprint('auth', __name__)
 
 
 # ------------------------ individual route start ------------------------
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login_function():
   return render_template('login.html')
 # ------------------------ individual route end ------------------------
@@ -30,7 +30,25 @@ def logout_function():
 
 
 # ------------------------ individual route start ------------------------
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up_function():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    firstName = request.form.get('firstName')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    if len(email) < 4:
+      flash('Email must be greater than 3 characters.', category='error')
+    elif len(firstName) < 2:
+      flash('First name must be greater than 1 character.', category='error')
+    elif password1 != password2:
+      flash('Passwords don\'t match.', category='error')
+    elif len(password1) < 7:
+      flash('Password must be at least 7 characters.', category='error')
+    else:
+      flash('Account created!', category='success')
+      pass
+
   return render_template('sign_up.html')
 # ------------------------ individual route end ------------------------
