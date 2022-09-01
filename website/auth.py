8e1f13be90eda09d9aefe3aa8c7985cf84ce0dc1
robert borowski <rborowski141@gmail.com>
2022-09-01 07:09:03 -0400

    login logic

--- website/auth.py
+++ website/auth.py
@@ -22,6 +22,18 @@ auth = Blueprint('auth', __name__)
 # ------------------------ individual route start ------------------------
 @auth.route('/login', methods=['GET', 'POST'])
 def login_function():
+  email = request.form.get('email')
+  password = request.form.get('password')
+  
+  user = User.query.filter_by(email=email).first()
+  if user:
+    if check_password_hash(user.password, password):
+      flash('Logged in successfully!', category='success')
+    else:
+      flash('Incorrect password, try again.', category='error')
+  else:
+    flash('Email does not exist.', category='error')
+
   return render_template('login.html')
 # ------------------------ individual route end ------------------------
 
@@ -42,7 +54,10 @@ def sign_up_function():
     password1 = request.form.get('password1')
     password2 = request.form.get('password2')
 
-    if len(email) < 4:
+    user = User.query.filter_by(email=email).first()
+    if user:
+      flash('Email already exists.', category='error')
+    elif len(email) < 4:
       flash('Email must be greater than 3 characters.', category='error')
     elif len(firstName) < 2:
       flash('First name must be greater than 1 character.', category='error')
