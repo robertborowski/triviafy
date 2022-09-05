219d907811312957817d183382d1e1841b62c020
robert borowski <rborowski141@gmail.com>
2022-09-04 21:14:51 -0400

    migrating candidates pages over to new flask code

--- website/views.py
+++ website/views.py
@@ -6,19 +6,69 @@
 
 
 # ------------------------ imports start ------------------------
+from backend.utils.uuid_and_timestamp.create_uuid import create_uuid_function
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 # ------------------------ imports end ------------------------
-
 
 # ------------------------ function start ------------------------
 views = Blueprint('views', __name__)
 # ------------------------ function end ------------------------
 
+# ------------------------ before page variables start ------------------------
+cache_busting_output = create_uuid_function('css_')
+# ------------------------ before page variables end ------------------------
 
-# ------------------------ individual route start ------------------------
+# ------------------------ routes start ------------------------
+# ------------------------ individual route - aaaa youtube start ------------------------
 @views.route('/')
 @login_required   # this decorator says that url cannot be accessed unless the user is logged in.
 def home_function():
   return render_template('home.html', user=current_user)
-# ------------------------ individual route end ------------------------
+# ------------------------ individual route - aaaa youtube end ------------------------
+# ------------------------ individual route - candidates about start ------------------------
+@views.route('/candidates/about')
+def candidates_about_page_function():
+  return render_template('candidates_page_templates/about_page_templates/index.html', user=current_user)
+# ------------------------ individual route - candidates about end ------------------------
+# ------------------------ individual route - candidates collect email start ------------------------
+@views.route('/candidates/email')
+def candidates_email_page_function():
+  return render_template('candidates_page_templates/collect_email_page_templates/index.html', user=current_user)
+# ------------------------ individual route - candidates collect email end ------------------------
+# ------------------------ individual route - candidates faq start ------------------------
+@views.route('/candidates/faq')
+def candidates_faq_page_function():
+  return render_template('candidates_page_templates/faq_page_templates/index.html', user=current_user)
+# ------------------------ individual route - candidates faq end ------------------------
+# ------------------------ individual route - candidates index main start ------------------------
+@views.route('/candidates')
+def landing_index_page_function():
+  return render_template('candidates_page_templates/index_page_templates/index.html', user=current_user)
+# ------------------------ individual route - candidates index main end ------------------------
+# ------------------------ individual route - candidates pricing start ------------------------
+@views.route('/candidates/pricing')
+def candidates_pricing_page_function():
+  return render_template('candidates_page_templates/pricing_page_templates/index.html', user=current_user)
+# ------------------------ individual route - candidates pricing end ------------------------
+# ------------------------ individual route - candidates stand in start ------------------------
+@views.route('/candidates/launch')
+def candidates_stand_in_page_function():
+  return render_template('candidates_page_templates/stand_in_page_templates/index.html', user=current_user)
+# ------------------------ individual route - candidates stand in end ------------------------
+# ------------------------ individual route - candidates test library start ------------------------
+@views.route('/candidates/library')
+def candidates_test_library_page_function():
+  return render_template('candidates_page_templates/test_library_page_templates/index.html', user=current_user)
+# ------------------------ individual route - candidates test library end ------------------------
+# ------------------------ individual route - privacy start ------------------------
+@views.route('/privacy')
+def privacy_page_function():
+  return render_template('privacy_policy_page_templates/index.html', user=current_user)
+# ------------------------ individual route - privacy end ------------------------
+# ------------------------ individual route - privacy start ------------------------
+@views.route('/tos')
+def terms_of_service_page_function():
+  return render_template('terms_of_service_page_templates/index.html', user=current_user)
+# ------------------------ individual route - privacy end ------------------------
+# ------------------------ routes end ------------------------
