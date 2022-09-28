22aeb2c0240509772d62014e9c7d5c71f36cbcfb
robert borowski <rborowski141@gmail.com>
2022-09-28 07:45:44 -0400

    dropdown naming changed

--- website/static/javascript/candidates_js/javascript_utils/form_js/multiple_select_dropdown.js
+++ website/static/javascript/candidates_js/javascript_utils/form_js/multiple_select_dropdown.js
@@ -1,5 +1,5 @@
-var checkList = document.getElementById('list1');
-checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
+var checkList = document.getElementById('create-assessment-choose-lang-list1');
+checkList.getElementsByClassName('create-assessment-anchor')[0].onclick = function(evt) {
   if (checkList.classList.contains('visible'))
     checkList.classList.remove('visible');
   else
