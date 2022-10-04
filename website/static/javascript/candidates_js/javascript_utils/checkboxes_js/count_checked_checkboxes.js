cc9d29584a977a1f8097fff9beae9e9718526485
robert borowski <rborowski141@gmail.com>
2022-10-04 16:42:30 -0400

    mock submit button built in and select only 10 questions

--- website/static/javascript/candidates_js/javascript_utils/checkboxes_js/count_checked_checkboxes.js
+++ website/static/javascript/candidates_js/javascript_utils/checkboxes_js/count_checked_checkboxes.js
@@ -1,4 +1,5 @@
 let selectQuestionsButtonSection = document.getElementsByClassName('select-questions-button-section');
+let selectQuestionsButtonStandInSection = document.getElementsByClassName('select-questions-button-stand-in-section');
 
 function count_checked_checkboxes(){
   var inputElems = document.getElementsByTagName("input"),
@@ -7,12 +8,14 @@ function count_checked_checkboxes(){
   if (inputElems[i].type === "checkbox" && inputElems[i].checked === true){
     checked_counter++;
     console.log(checked_counter)
-    if (checked_counter >= 10) {
+    if (checked_counter == 10) {
       selectQuestionsButtonSection[0].classList.add('active');
+      selectQuestionsButtonStandInSection[0].classList.add('hide-mock-button');
     }
-    if (checked_counter <= 9) {
+    if (checked_counter <= 9 || checked_counter >= 11) {
       try {
         selectQuestionsButtonSection[0].classList.remove('active');
+        selectQuestionsButtonStandInSection[0].classList.remove('hide-mock-button');
       }
       catch(err) {
         console.log('nothing')
