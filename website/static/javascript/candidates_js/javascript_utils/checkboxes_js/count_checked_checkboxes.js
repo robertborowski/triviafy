5090f321ce4950b17b85172fba9a1915af0162ec
robert borowski <rborowski141@gmail.com>
2022-10-07 08:05:19 -0400

    auto checkbox first 10 js

--- website/static/javascript/candidates_js/javascript_utils/checkboxes_js/count_checked_checkboxes.js
+++ website/static/javascript/candidates_js/javascript_utils/checkboxes_js/count_checked_checkboxes.js
@@ -1,25 +1,28 @@
 let selectQuestionsButtonSection = document.getElementsByClassName('select-questions-button-section');
 let selectQuestionsButtonStandInSection = document.getElementsByClassName('select-questions-button-stand-in-section');
 
+window.onload = count_checked_checkboxes();
+
 function count_checked_checkboxes(){
   var inputElems = document.getElementsByTagName("input"),
   checked_counter = 0;
   for (var i=0; i<inputElems.length; i++) {
-  if (inputElems[i].type === "checkbox" && inputElems[i].checked === true){
-    checked_counter++;
-    console.log(checked_counter)
-    if (checked_counter == 10) {
-      selectQuestionsButtonSection[0].classList.add('active');
-      selectQuestionsButtonStandInSection[0].classList.add('hide-mock-button');
-    }
-    if (checked_counter <= 9 || checked_counter >= 11) {
-      try {
-        selectQuestionsButtonSection[0].classList.remove('active');
-        selectQuestionsButtonStandInSection[0].classList.remove('hide-mock-button');
+    if (inputElems[i].type === "checkbox" && inputElems[i].checked === true) {
+      checked_counter++;
+      // console.log(checked_counter)
+      if (checked_counter == 10) {
+        selectQuestionsButtonSection[0].classList.add('active');
+        selectQuestionsButtonStandInSection[0].classList.add('hide-mock-button');
       }
-      catch(err) {
-        console.log('nothing')
+      if (checked_counter <= 9 || checked_counter >= 11) {
+        try {
+          selectQuestionsButtonSection[0].classList.remove('active');
+          selectQuestionsButtonStandInSection[0].classList.remove('hide-mock-button');
+        }
+        catch(err) {
+          console.log('nothing')
+        }
       }
     }
   }
-}}
+}
