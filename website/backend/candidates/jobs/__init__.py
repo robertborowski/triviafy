c1627132797515388358f20d87671a5ec5fb5904
robert borowski <rborowski141@gmail.com>
2022-10-23 13:14:35 -0400

    before merge

--- website/backend/candidates/jobs/__init__.py
+++
@@ -1,7 +0,0 @@
-# ------------------------ imports start ------------------------
-from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
-# ------------------------ imports end ------------------------
-
-
-localhost_print_function('=========================================== jobs __init__ START ===========================================')
-localhost_print_function('=========================================== jobs __init__ END ===========================================')
