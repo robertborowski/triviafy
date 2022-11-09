3627a705a90318824ef3aa66490197f71d3bdd6b
robert borowski <rborowski141@gmail.com>
2022-11-09 18:47:12 -0500

    page load checks priting

--- backend/utils/localhost_print_utils/localhost_print.py
+++ backend/utils/localhost_print_utils/localhost_print.py
@@ -4,7 +4,8 @@ import os
 
 # -------------------------------------------------------------- Main Function
 def localhost_print_function(input_value_to_print):
-  server_env = os.environ.get('TESTING', 'false')
+  # server_env = os.environ.get('TESTING', 'false')
+  server_env = os.environ.get('PRINTINGG', 'false')
   
   if server_env and server_env == 'true':
     print(input_value_to_print)
