5bb132636b56d60dee3064368f388cfa32448068
robert borowski <rborowski141@gmail.com>
2022-11-25 09:58:24 -0500

    new blog post job

--- jobs/candidates/email_jobs/__init__.py
+++ jobs/candidates/email_jobs/__init__.py
@@ -117,3 +117,61 @@ def job_candidates_email_all_users_function(postgres_connection, postgres_cursor
   return True
 # ------------------------ main end ------------------------
 # ------------------------ individual function end ------------------------
+
+
+# ------------------------ individual function start ------------------------
+# ------------------------ main start ------------------------
+def job_candidates_send_article_to_all_users_function(postgres_connection, postgres_cursor):
+  localhost_print_function('=========================================== job_candidates_send_article_to_all_users_function START ===========================================')
+  # ------------------------ pull from db start ------------------------
+  all_users_arr_signed_up = select_all_users_function(postgres_connection, postgres_cursor)
+  all_users_arr_collected_emails = select_all_collected_emails_function(postgres_connection, postgres_cursor)
+  # ------------------------ pull from db end ------------------------
+  # ------------------------ if none start ------------------------
+  if all_users_arr_signed_up == None or all_users_arr_collected_emails == None:
+    localhost_print_function('no emails to send out today')
+    localhost_print_function('=========================================== job_candidates_send_article_to_all_users_function END ===========================================')
+    return True
+  # ------------------------ if none end ------------------------
+  # ------------------------ get all emails arr start ------------------------
+  all_user_emails_arr = []
+  for i in all_users_arr_signed_up:
+    if i[2] not in all_user_emails_arr:
+      all_user_emails_arr.append(i[2])
+  for i in all_users_arr_collected_emails:
+    if i[2] not in all_user_emails_arr:
+      all_user_emails_arr.append(i[2])
+  # ------------------------ get all emails arr end ------------------------
+  # ------------------------ loop through each schedule item start ------------------------
+  failed_emails_arr = []
+  for i_email in all_user_emails_arr:
+    # ------------------------ email only self start ------------------------
+    run_test_email = os.environ.get('RUN_TEST_EMAIL')
+    if i_email != run_test_email:
+      print(f'skip: {i_email}')
+      continue
+    # ------------------------ email only self end ------------------------
+    try:
+      # ------------------------ send email start ------------------------
+      output_to_email = i_email
+      add_link = 'https://triviafy.com/candidates/blog/0001'
+      output_subject = f'5 Tools for Pre-Employment Screening Tests and Assessments - Triviafy'
+      output_body = f"Hi there,\n\nNew blog post from the friendliest pre employment testing platform. Share with your team {add_link}\n\nReply 'STOP' to opt out.\n\nBest,\nTriviafy"
+      send_email_template_function(output_to_email, output_subject, output_body)
+      # ------------------------ send email end ------------------------
+      # ------------------------ insert row db start ------------------------
+      row_id = create_uuid_function('email_ann_')
+      row_created_timestamp = create_timestamp_function()
+      insert_input_arr = [row_id, row_created_timestamp, 'job', output_to_email, 'announcement_blog_post', output_subject, output_body]
+      insert_email_sent_row_function(postgres_connection, postgres_cursor, insert_input_arr)
+      # ------------------------ insert row db end ------------------------
+      # ------------------------ convert to goal send time format end ------------------------
+    except:
+      failed_emails_arr.append(i_email)
+      pass
+  # ------------------------ loop through each schedule item end ------------------------
+  localhost_print_function(f'len_failed_emails_arr: {len(failed_emails_arr)} | failed_emails_arr: {failed_emails_arr}')
+  localhost_print_function('=========================================== job_candidates_send_article_to_all_users_function END ===========================================')
+  return True
+# ------------------------ main end ------------------------
+# ------------------------ individual function end ------------------------
