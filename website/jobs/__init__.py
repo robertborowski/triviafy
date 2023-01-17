d8d24e69bd9875670e427e49863d9fba2448e1b1
Robert <61035696+robertborowski@users.noreply.github.com>
2023-01-17 18:52:20 -0500

    back to OG

--- website/jobs/__init__.py
+++
@@ -1,9 +0,0 @@
-# ------------------------ imports start ------------------------
-from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
-# ------------------------ imports end ------------------------
-
-
-localhost_print_function('=========================================== jobs __init__ START ===========================================')
-# ------------------------ individual function start ------------------------
-# ------------------------ individual function end ------------------------
-localhost_print_function('=========================================== jobs __init__ END ===========================================')
