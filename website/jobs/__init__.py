568f6de25f523dba227ee1cf95c486058d998fb0
Robert <61035696+robertborowski@users.noreply.github.com>
2023-01-17 18:38:18 -0500

    job test need to switch branch

---
+++ website/jobs/__init__.py
@@ -0,0 +1,9 @@
+# ------------------------ imports start ------------------------
+from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
+# ------------------------ imports end ------------------------
+
+
+localhost_print_function('=========================================== jobs __init__ START ===========================================')
+# ------------------------ individual function start ------------------------
+# ------------------------ individual function end ------------------------
+localhost_print_function('=========================================== jobs __init__ END ===========================================')
