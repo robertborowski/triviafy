e35767f9c33412405b0fbf0b19b0992fa25adeab
Robert <61035696+robertborowski@users.noreply.github.com>
2023-01-27 18:41:51 -0500

    trim search bar input

--- website/static/javascript/all_js/search_bar_live_html/function.js
+++ website/static/javascript/all_js/search_bar_live_html/function.js
@@ -1,9 +1,11 @@
 $("#uiSearch").keyup(function(){
   var currently_typed = $(this).val().toLowerCase();
+  currently_typed = $.trim(currently_typed)
 
   var testList = $('.uiSearchItem');
   testList.each(function() {
     var i_category = $(this).text().toLowerCase();
+    i_category = $.trim(i_category)
     // if (i_category == "aws")
     if (i_category.indexOf(currently_typed) >= 0) {
       //  block of code to be executed if the condition is true
