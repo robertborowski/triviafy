18dc0df5620debd83643c8c891cbb4e8258c2c16
Robert <61035696+robertborowski@users.noreply.github.com>
2023-01-31 07:40:49 -0500

    example working

--- website/employees_views_exterior.py
+++ website/employees_views_exterior.py
@@ -9,10 +9,12 @@
 
 # ------------------------ imports start ------------------------
 from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
 from website.backend.candidates.redis import redis_connect_to_database_function
 from website.models import CreatedQuestionsObj, CandidatesAssessmentsCreatedObj
+from website.backend.candidates.dict_manipulation import arr_of_dict_all_columns_single_item_function, categories_tuple_function
+from website.backend.candidates.user_inputs import alert_message_default_function_v2
 # ------------------------ imports end ------------------------
 
 # ------------------------ function start ------------------------
@@ -81,12 +83,43 @@ def employees_i_blog_page_function(i_blog_post_number='0001'):
 # ------------------------ individual route start ------------------------
 @employees_views_exterior.route('/employees/example', methods=['GET', 'POST'])
 @employees_views_exterior.route('/employees/example/', methods=['GET', 'POST'])
-def employees_example_page_function():
+@employees_views_exterior.route('/employees/example/<url_question_number>', methods=['GET', 'POST'])
+@employees_views_exterior.route('/employees/example/<url_question_number>/', methods=['GET', 'POST'])
+def employees_example_page_function(url_redirect_code=None, url_question_number='1'):
   localhost_print_function(' ------------------------ employees_example_page_function start ------------------------ ')
+  # ------------------------ redirect codes start ------------------------
+  alert_message_dict = alert_message_default_function_v2(url_redirect_code)
+  # ------------------------ redirect codes end ------------------------
+  # ------------------------ pull test start ------------------------
   db_test_obj = CandidatesAssessmentsCreatedObj.query.filter_by(id='employees_example_do_not_delete_01').first()
-  localhost_print_function(' ------------- 0 ------------- ')
-  localhost_print_function(f'db_test_obj | type: {type(db_test_obj)} | {db_test_obj}')
-  localhost_print_function(' ------------- 0 ------------- ')
+  db_test_obj = arr_of_dict_all_columns_single_item_function(db_test_obj)
+  total_questions = int(db_test_obj['total_questions'])
+  try:
+    if int(url_question_number) < 1 or int(url_question_number) > total_questions:
+      return redirect(url_for('employees_views_exterior.employees_example_page_function', url_question_number='1'))
+  except:
+    return redirect(url_for('employees_views_exterior.employees_example_page_function', url_question_number='1'))
+  question_ids_str = db_test_obj['question_ids_arr']
+  question_ids_arr = question_ids_str.split(',')
+  desired_question_str = question_ids_arr[int(url_question_number)-1]
+  # ------------------------ pull test end ------------------------
+  # ------------------------ pull question start ------------------------
+  db_question_obj = CreatedQuestionsObj.query.filter_by(id=desired_question_str).first()
+  db_question_obj = arr_of_dict_all_columns_single_item_function(db_question_obj)
+  db_question_obj['categories'] = categories_tuple_function(db_question_obj['categories'])
+  # ------------------------ pull question end ------------------------
+  # ------------------------ previous next current start ------------------------
+  previous_question_number = str(int(url_question_number) - 1)
+  current_question_number = str(url_question_number)
+  next_question_number = str(int(url_question_number) + 1)
+  if int(url_question_number) == total_questions:
+    next_question_number = 'submit'
+  # ------------------------ previous next current end ------------------------
+  # ------------------------ check if contains img start ------------------------
+  contains_img = False
+  if 'amazonaws.com' in db_question_obj['aws_image_url']:
+    contains_img = True
+  # ------------------------ check if contains img end ------------------------
   localhost_print_function(' ------------------------ employees_example_page_function end ------------------------ ')
-  return render_template('employees/exterior/example/index.py')
+  return render_template('employees/exterior/example_test/index.html', db_question_obj_to_html=db_question_obj, previous_question_number_to_html=previous_question_number, current_question_number_to_html=current_question_number, next_question_number_to_html=next_question_number, alert_message_dict_to_html=alert_message_dict, contains_img_to_html=contains_img)
 # ------------------------ individual route end ------------------------
