9642c128a3b41b3b326a6ea8b05027272f673e0b
Robert <61035696+robertborowski@users.noreply.github.com>
2023-02-25 18:35:07 -0500

    job query working

--- backend/db/queries/select_queries/employees/__init__.py
+++ backend/db/queries/select_queries/employees/__init__.py
@@ -1,13 +1,34 @@
 # ------------------------ imports start ------------------------
 from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
+import psycopg2
+from psycopg2 import Error, extras
 # ------------------------ imports end ------------------------
 
 localhost_print_function(' ------------------------ select_queries employees __init__ start ------------------------ ')
 # ------------------------ individual function start ------------------------
-def select_general_function():
-  localhost_print_function(' ------------------------ select_general_function start ------------------------ ')
-  localhost_print_function('hello')
-  localhost_print_function(' ------------------------ select_general_function end ------------------------ ')
-  return True
+def select_manual_function(postgres_connection, postgres_cursor, tag_query_to_use, additional_input=None, additional_input2=None, additional_input3=None):
+  localhost_print_function(' ------------------------ select_manual_function start ------------------------ ')
+  # ------------------------ select queries start ------------------------
+  select_queries_dict = {
+    'select_groups_1':
+      f"SELECT \
+          fk_company_name, \
+          public_group_id \
+        FROM \
+          employees_groups_obj;"
+  }
+  # ------------------------ select queries end ------------------------
+  # ------------------------ cursor start ------------------------
+  cursor = postgres_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+  cursor.execute(select_queries_dict[tag_query_to_use])
+  # ------------------------ cursor end ------------------------
+  # ------------------------ results start ------------------------
+  results_arr = cursor.fetchall()
+  result_arr_dicts = []
+  for row in results_arr:
+    result_arr_dicts.append(dict(row))
+  # ------------------------ results end ------------------------
+  localhost_print_function(' ------------------------ select_manual_function end ------------------------ ')
+  return result_arr_dicts
 # ------------------------ individual function end ------------------------
 localhost_print_function(' ------------------------ select_queries employees __init__ end ------------------------ ')
--- jobs/employees_quiz_open_close_notifications.py
+++ jobs/employees_quiz_open_close_notifications.py
@@ -2,7 +2,7 @@
 from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
 from backend.db.connection.postgres_connect_to_database import postgres_connect_to_database_function
 from backend.db.connection.postgres_close_connection_to_database import postgres_close_connection_to_database_function
-from backend.db.queries.select_queries.employees import select_general_function
+from backend.db.queries.select_queries.employees import select_manual_function
 # ------------------------ imports end ------------------------
 
 # ------------------------ individual function start ------------------------
@@ -12,7 +12,7 @@ def employees_quiz_open_close_notifications():
   postgres_connection, postgres_cursor = postgres_connect_to_database_function()
   # ------------------------ open connection end ------------------------
   # ------------------------ pull all groups start ------------------------
-  test = select_general_function()
+  db_groups_arr_of_dict = select_manual_function(postgres_connection, postgres_cursor, 'select_groups_1')
   # ------------------------ pull all groups end ------------------------
   # ------------------------ close connection start ------------------------
   postgres_close_connection_to_database_function(postgres_connection, postgres_cursor)
