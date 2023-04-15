be277f72b37e35f94955bfea530ed0668c28f5a9
Robert <61035696+robertborowski@users.noreply.github.com>
2023-04-15 17:47:21 -0400

    pricing better

--- website/static/javascript/all_js/checkbox_js/switch_v1.js
+++ website/static/javascript/all_js/checkbox_js/switch_v1.js
@@ -3,12 +3,48 @@ $(document).ready(function() {
   $('input[name="flexRadioCadence"]').change(function() {
     if ($(this).is(':checked')) {
       if ($(this).val() == 'monthly') {
-        $('#flexRadioMonthly').addClass('uiSearchItemVisible');
-        $('#flexRadioYearly').removeClass('uiSearchItemVisible');
+        // ------------------------ price 1 start ------------------------
+        $('#flexRadioMonthlyPrice1').addClass('uiSearchItemVisible');
+        $('#flexRadioMonthlyPrice1').removeClass('uiSearchItemInvisible');
+        $('#flexRadioYearlyPrice1').addClass('uiSearchItemInvisible');
+        $('#flexRadioYearlyPrice1').removeClass('uiSearchItemVisible');
+        // ------------------------ price 1 end ------------------------
+        // ------------------------ price 2 start ------------------------
+        $('#flexRadioMonthlyPrice2').addClass('uiSearchItemVisible');
+        $('#flexRadioMonthlyPrice2').removeClass('uiSearchItemInvisible');
+        $('#flexRadioYearlyPrice2').addClass('uiSearchItemInvisible');
+        $('#flexRadioYearlyPrice2').removeClass('uiSearchItemVisible');
+        // ------------------------ price 2 end ------------------------
+        // ------------------------ selection start ------------------------
+        $('#flexRadioSelectMonthly').addClass('custom-bg-primary');
+        $('#flexRadioSelectMonthly').addClass('fw-bold');
+        $('#flexRadioSelectMonthly').addClass('custom-color-white');
+        $('#flexRadioSelectYearly').removeClass('custom-bg-primary');
+        $('#flexRadioSelectYearly').removeClass('fw-bold');
+        $('#flexRadioSelectYearly').removeClass('custom-color-white');
+        // ------------------------ selection end ------------------------
       }
       if ($(this).val() == 'yearly') {
-        $('#flexRadioYearly').addClass('uiSearchItemVisible');
-        $('#flexRadioMonthly').removeClass('uiSearchItemVisible');
+        // ------------------------ price 1 start ------------------------
+        $('#flexRadioMonthlyPrice1').addClass('uiSearchItemInvisible');
+        $('#flexRadioMonthlyPrice1').removeClass('uiSearchItemVisible');
+        $('#flexRadioYearlyPrice1').addClass('uiSearchItemVisible');
+        $('#flexRadioYearlyPrice1').removeClass('uiSearchItemInvisible');
+        // ------------------------ price 1 end ------------------------
+        // ------------------------ price 2 start ------------------------
+        $('#flexRadioMonthlyPrice2').addClass('uiSearchItemInvisible');
+        $('#flexRadioMonthlyPrice2').removeClass('uiSearchItemVisible');
+        $('#flexRadioYearlyPrice2').addClass('uiSearchItemVisible');
+        $('#flexRadioYearlyPrice2').removeClass('uiSearchItemInvisible');
+        // ------------------------ price 2 end ------------------------
+        // ------------------------ selection start ------------------------
+        $('#flexRadioSelectMonthly').removeClass('custom-bg-primary');
+        $('#flexRadioSelectMonthly').removeClass('fw-bold');
+        $('#flexRadioSelectMonthly').removeClass('custom-color-white');
+        $('#flexRadioSelectYearly').addClass('custom-bg-primary');
+        $('#flexRadioSelectYearly').addClass('fw-bold');
+        $('#flexRadioSelectYearly').addClass('custom-color-white');
+        // ------------------------ selection end ------------------------
       }
     }
   });
