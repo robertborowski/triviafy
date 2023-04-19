7bf25d8acef85c9a4caf8a6487bdb5d9e72fd9cf
Robert <61035696+robertborowski@users.noreply.github.com>
2023-04-19 18:33:59 -0400

    small

--- website/backend/candidates/lists/__init__.py
+++ website/backend/candidates/lists/__init__.py
@@ -5,7 +5,7 @@ from backend.utils.localhost_print_utils.localhost_print import localhost_print_
 localhost_print_function('=========================================== lists __init__ START ===========================================')
 # ------------------------ individual function start ------------------------
 def get_team_building_activities_list_function():
-  activities_list = ['Trivia', 'Icebreakers', 'Employee surveys', 'Personality tests', 'Picture quizzes', 'Colleague birthday trivia', 'Most likely to', 'This or that', 'Watercooler starters', 'Word games', '2 player games', 'Create custom questions']
+  activities_list = ['Trivia', 'Picture quizzes', 'Icebreakers', 'Employee surveys', 'Personality tests', 'Colleague birthday trivia', 'Most likely to', 'This or that', 'Watercooler starters', 'Word games', '2 player games', 'Create custom questions']
   # activities_list = sorted(activities_list)
   # ------------------------ get counter start ------------------------
   activities_list_index = []
