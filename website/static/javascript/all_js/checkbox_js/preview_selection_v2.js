3580bcd34bf563572c2ef74f9f05a1076014b3d0
Robert <61035696+robertborowski@users.noreply.github.com>
2023-04-23 19:18:53 -0400

    checkboxes working for selecting categories

--- website/static/javascript/all_js/checkbox_js/preview_selection_v2.js
+++ website/static/javascript/all_js/checkbox_js/preview_selection_v2.js
@@ -22,3 +22,43 @@ function previewBirthdayDaySelection(obj){
   }
 }
 // ----------------------- end -----------------------
+// ----------------------- start -----------------------
+$(document).ready(function() {
+  // ----------------------- remove space id names start -----------------------
+  $('*').each(function() {
+    var id = $(this).attr('id');
+    if (id && id.includes(' ')) {
+      var newId = id.replace(/\s+/g, ''); // remove any spaces from the id
+      $(this).attr('id', newId); // update the id attribute value
+    }
+    var forId = $(this).attr('for');
+    if (forId && forId.includes(' ')) {
+      var newForId = forId.replace(/\s+/g, ''); // remove any spaces from the id
+      $(this).attr('for', newForId); // update the id attribute value
+    }
+  });
+  // ----------------------- remove space id names end -----------------------
+  // ----------------------- checked vs unchecked classes start -----------------------
+  $('input[type=checkbox][name=uiSelectedCategories]:checked').each(function() {
+    var inputId = $(this).attr('id'); // get the ID of the input element
+    var label = $('label[for=' + inputId + ']'); // select the associated label
+    label.addClass('custom-highlight-1'); // add the custom class to the label
+  });
+  $('input[type=checkbox][name=uiSelectedCategories]:not(:checked)').each(function() {
+    var inputId = $(this).attr('id'); // get the ID of the input element
+    var label = $('label[for=' + inputId + ']'); // select the associated label
+    label.removeClass('custom-highlight-1'); // add the custom class to the label
+  });
+});
+function highlightCheckboxV1(obj){
+  $('input[type=checkbox][name=uiSelectedCategories]:checked').change(function() {
+    var checkboxId = $(this).prop('id');
+    $('label[for=' + checkboxId + ']').addClass('custom-highlight-1');
+  });
+  $('input[type=checkbox][name=uiSelectedCategories]:not(:checked)').change(function() {
+    var checkboxId = $(this).prop('id');
+    $('label[for=' + checkboxId + ']').removeClass('custom-highlight-1');
+  });
+  // ----------------------- checked vs unchecked classes end -----------------------
+}
+// ----------------------- end -----------------------
