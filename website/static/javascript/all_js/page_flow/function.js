bcc8a6257919d60a1aa731ed49c680907548db91
Robert <61035696+robertborowski@users.noreply.github.com>
2023-04-24 19:09:59 -0400

    show details working toggle

--- website/static/javascript/all_js/page_flow/function.js
+++ website/static/javascript/all_js/page_flow/function.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // ----------------------------- continue form start -----------------------------
   $("#id-flow_continue_1").click(function() {
     // part 1
     $("#id-form_part_1").removeClass("uiSearchItemVisible2");
@@ -13,4 +14,25 @@ $(document).ready(function() {
     $("#id-flow_submit_1").removeClass("uiSearchItemInvisible");
     $("#id-flow_submit_1").addClass("uiSearchItemVisible2");
   });
+  // ----------------------------- continue form end -----------------------------
+  // ----------------------------- activity details start -----------------------------
+  // show info
+  $("#id-flow_show_details_1").click(function() {
+    $("#id-activity_details_1").removeClass("uiSelectAllBlockInvisible");
+    $("#id-activity_details_1").addClass("uiSelectAllBlockVisible");
+    $("#id-flow_hide_details_1").removeClass("uiSelectAllBlockInvisible");
+    $("#id-flow_hide_details_1").addClass("uiSelectAllBlockVisible");
+    $("#id-flow_show_details_1").removeClass("uiSelectAllBlockVisible");
+    $("#id-flow_show_details_1").addClass("uiSelectAllBlockInvisible");
+  });
+  // hide info
+  $("#id-flow_hide_details_1").click(function() {
+    $("#id-activity_details_1").removeClass("uiSelectAllBlockVisible");
+    $("#id-activity_details_1").addClass("uiSelectAllBlockInvisible");
+    $("#id-flow_hide_details_1").removeClass("uiSelectAllBlockVisible");
+    $("#id-flow_hide_details_1").addClass("uiSelectAllBlockInvisible");
+    $("#id-flow_show_details_1").removeClass("uiSelectAllBlockInvisible");
+    $("#id-flow_show_details_1").addClass("uiSelectAllBlockVisible");
+  });
+  // ----------------------------- activity details end -----------------------------
 });
