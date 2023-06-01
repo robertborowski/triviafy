9484f91f4575d300df500c49371ab28bed051187
Robert <61035696+robertborowski@users.noreply.github.com>
2023-06-01 07:43:01 -0400

    small test

--- website/backend/candidates/sql_statements/sql_statements_select/__init__.py
+++ website/backend/candidates/sql_statements/sql_statements_select/__init__.py
@@ -207,7 +207,7 @@ def select_general_function(tag_query_to_use, additional_input=None, additional_
       'input_args': {'val': additional_input}
     },
     'select_sample_trivia': {
-      'raw_query': "SELECT \
+      'raw_query': f"SELECT \
                       * \
                     FROM \
                       activity_a_created_questions_obj \
@@ -217,7 +217,7 @@ def select_general_function(tag_query_to_use, additional_input=None, additional_
                     ORDER BY \
                       created_timestamp DESC \
                     LIMIT 5;",
-      'input_args': {}
+      'input_args': {'val': additional_input}
     },
     'select_sample_picture_quiz': {
       'raw_query': f"SELECT \
