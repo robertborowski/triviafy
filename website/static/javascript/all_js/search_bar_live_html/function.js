7dae4a8a85504884c9bab73a41b717cc5c8a03d6
Robert <61035696+robertborowski@users.noreply.github.com>
2023-06-13 18:34:58 -0400

    dynamic live search

--- website/static/javascript/all_js/search_bar_live_html/function.js
+++ website/static/javascript/all_js/search_bar_live_html/function.js
@@ -1,3 +1,4 @@
+// -------------------------- start --------------------------
 $("#uiSearch").keyup(function(){
   var currently_typed = $(this).val().toLowerCase();
   currently_typed = $.trim(currently_typed)
@@ -18,3 +19,26 @@ $("#uiSearch").keyup(function(){
     }
   });
 });
+// -------------------------- end --------------------------
+// -------------------------- start --------------------------
+$("#ui_search_show_name").keyup(function(){
+  var currently_typed = $(this).val().toLowerCase();
+  currently_typed = $.trim(currently_typed)
+
+  var testList = $('.uiSearchItem');
+  testList.each(function() {
+    var i_category = $(this).text().toLowerCase();
+    i_category = $.trim(i_category)
+    // if (i_category == "aws")
+    if (i_category.indexOf(currently_typed) >= 0) {
+      //  block of code to be executed if the condition is true
+      $( this ).removeClass("uiSearchItemInvisible");
+      $( this ).addClass("uiSearchItemVisible2");
+    } else {
+      //  block of code to be executed if the condition is false
+      $( this ).removeClass("uiSearchItemVisible2");
+      $( this ).addClass("uiSearchItemInvisible");
+    }
+  });
+});
+// -------------------------- end --------------------------
