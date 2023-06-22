23ee744471b56c09ea1eb9186d613274168d9ecd
Robert <61035696+robertborowski@users.noreply.github.com>
2023-06-22 08:28:48 -0400

    chart 1 working for answer distribution

--- website/backend/poll_statistics/__init__.py
+++ website/backend/poll_statistics/__init__.py
@@ -14,6 +14,9 @@ def get_poll_statistics(current_user, page_dict):
   page_dict['poll_statistics_dict']['total_latest_poll_answers'] = 0
   page_dict['poll_statistics_dict']['answer_choice_count_distribution_dict'] = {}
   page_dict['poll_statistics_dict']['answer_choice_percent_distribution_dict'] = {}
+  page_dict['poll_statistics_dict']['chart_answer_distribution'] = {}
+  page_dict['poll_statistics_dict']['chart_answer_distribution']['labels'] = []
+  page_dict['poll_statistics_dict']['chart_answer_distribution']['values'] = []
   # ------------------------ set variables end ------------------------
   # ------------------------ get total answered start ------------------------
   total_answered_arr_of_dict = select_general_function('select_query_general_4', poll_id)
@@ -23,12 +26,16 @@ def get_poll_statistics(current_user, page_dict):
     pass
   # ------------------------ get total answered end ------------------------
   # ------------------------ loop answered count start ------------------------
+  for k,v in page_dict['poll_dict']['answer_choices_dict'].items():
+    page_dict['poll_statistics_dict']['answer_choice_count_distribution_dict'][k] = 0
   for i_poll_answered_dict in total_answered_arr_of_dict:
     i_poll_answer_submitted = i_poll_answered_dict['poll_answer_submitted']
     for k,v in page_dict['poll_dict']['answer_choices_dict'].items():
-      page_dict['poll_statistics_dict']['answer_choice_count_distribution_dict'][k] = 0
       if i_poll_answer_submitted == v:
-        page_dict['poll_statistics_dict']['answer_choice_count_distribution_dict'][k] += 1
+        try:
+          page_dict['poll_statistics_dict']['answer_choice_count_distribution_dict'][k] += 1
+        except:
+          pass
   # ------------------------ loop answered count start ------------------------
   # ------------------------ loop answered percent start ------------------------
   for k, v in page_dict['poll_statistics_dict']['answer_choice_count_distribution_dict'].items():
@@ -39,5 +46,10 @@ def get_poll_statistics(current_user, page_dict):
       pass
     page_dict['poll_statistics_dict']['answer_choice_percent_distribution_dict'][k] = int(result)
   # ------------------------ loop answered percent end ------------------------
+  # ------------------------ variables for chart start ------------------------
+  for k,v in page_dict['poll_statistics_dict']['answer_choice_percent_distribution_dict'].items():
+    page_dict['poll_statistics_dict']['chart_answer_distribution']['labels'].append(k)
+    page_dict['poll_statistics_dict']['chart_answer_distribution']['values'].append(v)
+  # ------------------------ variables for chart end ------------------------
   return page_dict
 # ------------------------ individual function end ------------------------
