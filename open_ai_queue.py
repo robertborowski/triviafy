26794d9aec2d28bd51b511073227de76599fbe3d
Robert <61035696+robertborowski@users.noreply.github.com>
2023-07-04 19:51:51 -0400

    testig

--- open_ai_queue.py
+++ open_ai_queue.py
@@ -111,19 +111,8 @@ def create_openai_starter_poll_questions_function(show_name):
   # ------------------------ clean show name end ------------------------
   # ------------------------ openai start ------------------------
   chatgpt_message = f"I would like you to create 10 polling questions for me that follow the following rules. Rule #1: The polling questions should be aimed at the average listener of the podcast '{show_name}'. Rule #2: Please avoid general polling questions that could be asked about any other podcast. Rule #3: Each polling question should have at least 5 options as answer choices. Rule #4: Each question number should be preceded with the word 'polling_question_' (lowercase), for example question 1 should be titled 'polling_question_1' (lowercase) and so on until 'polling_question_10' (lowercase). Rule #5: Each answer option should be preceded with the word 'polling_answer_' (lowercase), for example answer option 1 should be titled 'polling_answer_1' (lowercase) and so on until 'polling_answer_5' (lowercase). Rule #6: Do not include 'a) ,b) ,c) ,d) ,e) ' as separators for the answer choices. Rule #7: Do not include '-' as separators for the answer choices. Rule #8: The only answer choice separators should be polling_answer_1 through polling_answer_5. Rule #9: Each question number should not have any additional separators or numbering except for polling_question_1 through polling_question_10."
-  chatgpt_response_str = None
-  chatgpt_response_arr_of_dicts = None
-  error_message_caught = ''
-  try:
-    chatgpt_response_str = openai_chat_gpt_prompt_result_function(chatgpt_message)
-  except Exception as e:
-    error_message_caught = e
-    print(f'e1 error is: {e}')
-  try:
-    chatgpt_response_arr_of_dicts = openai_chat_gpt_parse_results_to_arr_polling_function(chatgpt_response_str)
-  except Exception as e:
-    error_message_caught = e
-    print(f'e2 error is: {e}')
+  chatgpt_response_str = openai_chat_gpt_prompt_result_function(chatgpt_message)
+  chatgpt_response_arr_of_dicts = openai_chat_gpt_parse_results_to_arr_polling_function(chatgpt_response_str)
   # ------------------------ openai end ------------------------
   # ------------------------ sanitize/check results as expected start ------------------------
   if chatgpt_response_arr_of_dicts == None or chatgpt_response_arr_of_dicts == []:
@@ -131,12 +120,11 @@ def create_openai_starter_poll_questions_function(show_name):
     try:
       output_to_email = os.environ.get('TRIVIAFY_NOTIFICATIONS_EMAIL')
       output_subject = f'Failure on ChatGPT parsing polls for {show_name}'
-      output_body = f'Failure on ChatGPT parsing polls for {show_name} | error_message_caught: {error_message_caught}'
+      output_body = f'Failure on ChatGPT parsing polls for {show_name}'
       send_email_template_function(output_to_email, output_subject, output_body)
     except:
       pass
     # ------------------------ email self end ------------------------
-    return None
   # ------------------------ sanitize/check results as expected end ------------------------
   return chatgpt_response_arr_of_dicts
 # ------------------------ individual function end ------------------------
@@ -186,18 +174,12 @@ def run_function():
               insert_manual_function(postgres_connection, postgres_cursor, 'insert_poll', insert_inputs_arr)
               # ------------------------ insert to db end ------------------------
             # ------------------------ add to db end ------------------------
-            # ------------------------ insert to db start ------------------------
-            created_timestamp=create_timestamp_function()
-            insert_inputs_arr = [new_show_id,created_timestamp,i_queue_dict['name'],i_queue_dict['description'],i_queue_dict['fk_platform_id'],True,i_queue_dict['platform_reference_id'],i_queue_dict['img_large'],i_queue_dict['img_medium'],i_queue_dict['img_small'],i_queue_dict['show_url']]
-            insert_manual_function(postgres_connection, postgres_cursor, 'insert_show', insert_inputs_arr)
-            # ------------------------ insert to db end ------------------------
-            # ------------------------ delete show from queue start ------------------------
-            try:
-              delete_manual_function(postgres_connection, postgres_cursor, 'delete_queue', i_queue_dict['id'])
-            except:
-              pass
-            # ------------------------ delete show from queue end ------------------------
           # ------------------------ openai get starter polls end ------------------------
+          # ------------------------ insert to db start ------------------------
+          created_timestamp=create_timestamp_function()
+          insert_inputs_arr = [new_show_id,created_timestamp,i_queue_dict['name'],i_queue_dict['description'],i_queue_dict['fk_platform_id'],True,i_queue_dict['platform_reference_id'],i_queue_dict['img_large'],i_queue_dict['img_medium'],i_queue_dict['img_small'],i_queue_dict['show_url']]
+          insert_manual_function(postgres_connection, postgres_cursor, 'insert_show', insert_inputs_arr)
+          # ------------------------ insert to db end ------------------------
         except:
           try:
             delete_manual_function(postgres_connection, postgres_cursor, 'delete_polls', new_show_id)
@@ -211,6 +193,12 @@ def run_function():
             delete_manual_function(postgres_connection, postgres_cursor, 'delete_shows_following', new_show_id)
           except:
             pass
+        # ------------------------ delete show from queue start ------------------------
+        try:
+          delete_manual_function(postgres_connection, postgres_cursor, 'delete_queue', i_queue_dict['id'])
+        except:
+          pass
+        # ------------------------ delete show from queue end ------------------------
         # ------------------------ openai rate limit start ------------------------
         time.sleep(30)
         # ------------------------ openai rate limit end ------------------------
